5d26fb13f39fa744d4e2b80e28ff2d0a9c00cae8
Luis Santos <lrosa-do@student.42lisboa.com>
2024-04-26 12:21:09 +0100

    fix menu animation

--- js/main.js
+++ js/main.js
@@ -1,24 +1,55 @@
 
 let isPressed= false;
 let doAnimate = false;
-
+ 
 
 document.addEventListener('DOMContentLoaded', function() {
    
     let menuIcon = document.querySelector("#menu-icon");
     let navbar = document.querySelector(".navbar");
-    
+    let isActive = false; 
     
     menuIcon.onclick = () => 
     {
       
         menuIcon.classList.toggle("fa-xmark");
-        navbar.classList.toggle("active");
-        console.log("doom clicked");
-        doAnimate = true;
-        isPressed=true;
+  
+        if (!isActive) 
+        {
+          navbar.classList.add("active");
+          navbar.classList.remove("hidden");
+          isActive = true;
+      } else 
+      {
+         // navbar.classList.remove("active");
+          navbar.classList.add("hidden");
+          isActive = false;
+      }
+
     
     }
+
+    document.addEventListener('click', function(event) 
+    {
+        if (!navbar.contains(event.target) && !menuIcon.contains(event.target)) 
+        {
+          
+          
+          //   navbar.classList.remove("active");
+              navbar.classList.add("hidden");
+              menuIcon.classList.remove("fa-xmark");
+              isActive = false;
+        }
+        if (navbar.contains(event.target)) 
+        {
+          
+          
+          //   navbar.classList.remove("active");
+              navbar.classList.add("hidden");
+              menuIcon.classList.remove("fa-xmark");
+              isActive = false;
+        }
+  });
 
     navbar.addEventListener('mouseover', () => 
     {
@@ -28,23 +59,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     navbar.addEventListener('mouseout', () => 
     {
-      if(isPressed)
-      {
-        isPressed    = false;
-        menuIcon.classList.toggle("fa-xmark");
-      }
-        navbar.classList.toggle("active");
-        navbar.classList.add("show");
-    });
-    // setTimeout(() => 
-    // {
-    //     navbar.classList.add("hidden");
-    //     menuIcon.classList.toggle("fa-xmark");
-    //     isPressed    = false;
-   
-     
-    // }, 3000);
 
- 
+    });
+
+
+    navbar.addEventListener('transitionend', function(event) 
+    {
+      
+     
+  });
 
  });
